96dd806a0d99c175378070ce5995a3247bfad7bd
Anushka Bachhav <anushkabachhav62@gmail.com>
2024-08-17 18:19:19 +0530

    tailwind and props

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  //let myObj= {name:"Anushka",id:1};
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
+      {/*<Card username={myObj}/>*/}
+      <Card username="Anushka" btnText='Click'/>
+      <Card username='Tanvi' btnText='View'/>
+    </>
+  )
+}
+
+export default App
