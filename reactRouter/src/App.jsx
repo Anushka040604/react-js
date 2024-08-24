4dc205e07a0ef4ecd4c28c6f7e21069624c6084c
Anushka Bachhav <anushkabachhav62@gmail.com>
2024-08-25 02:41:00 +0530

    Routes Route

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header/Header'
+import { useState } from 'react';
+//import './App.css';
+import Header from './components/Header/Header';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import { BrowserRouter, Route,Routes} from 'react-router-dom'; 
+import User from './pages/User';
+import Github, { githubInfoLoader } from './pages/Github';
 
 function App() {
-
-
   return (
-    <>
-    <h1 className='bg-green-600 p-4'>React Router</h1>
-    <Header/>
-    </>
-  )
+    <BrowserRouter>
+    <Header />
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path='user/:id' element={<User/>}/>
+          <Route 
+          loader={githubInfoLoader}
+          path='github' element={<Github/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ reactRouter/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Welcome to the homepage! This is where you can find the latest updates and information.</p>
+      {/* Add more content as needed */}
+    </>
+  );
+}
+
+export default Home;
